c71fa9fe31fc282ccd65c8d7366157a4bcecf9c4
KevinDev115 <galindok829@gmail.com>
2021-09-21 20:09:54 -0500

    avatar page

--- lib/src/pages/avatar_page.dart
+++ lib/src/pages/avatar_page.dart
@@ -6,6 +6,29 @@ class AvatarPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Avatar page'),
+        actions: <Widget>[
+          Container(
+            padding: EdgeInsets.all(7.0),
+            child: CircleAvatar(
+              backgroundImage: NetworkImage(
+                  'https://media.revistagq.com/photos/5ca5f0f8f552a13d4032e13c/master/pass/stan_lee_9628.jpg'),
+              radius: 30.0,
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(right: 10.0),
+            child: CircleAvatar(
+              child: Text('KG'),
+              backgroundColor: Colors.brown,
+            ),
+          )
+        ],
+      ),
+      body: Center(
+        child: FadeInImage(
+            placeholder: AssetImage('assets/img/jar-loading.gif'),
+            image: NetworkImage(
+                'https://media.revistagq.com/photos/5ca5f0f8f552a13d4032e13c/master/pass/stan_lee_9628.jpg')),
       ),
     );
   }
